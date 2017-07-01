a5c3b602250a2f00e651b441afb42ffe12ca4163
A Goemans <kewlboontjies@gmail.com>
2017-07-01 13:03:36 +0200

    created a temp js file

---
+++ data/movieData.js
@@ -0,0 +1,5 @@
+var movies = [
+    //title, date, dir, main star, co star, writer, rating, release date
+    ['It comes at Night', '2017', 'Trey Edward Shuits', 'Joel Edgerton', 'Christopher Abbott, Carmen Ejogo', 'Trey Edward Shuits', 'R', '09-06-2017'],
+    ['Totem', '2015', 'Ramon Llamas', 'Eric Aguirre', 'Tommy Kennedy, Patrick Southerland', 'Jazmine Martin', 'None', '19-10-2015']
+];
